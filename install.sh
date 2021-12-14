04f490520ebad7b950fa1cae66564e00a99e305b
rellax-net <tsangandrazana@outlook.com>
2021-12-14 19:59:42 +0000

    Added i3 lock

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 mkdir -p "$HOME/.config/nvim"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
-ln -sf "$HOME/dotfiles/i3/config" "$HOME/.config/i3"
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
