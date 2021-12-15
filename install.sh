4bd7a99f365a799a583565e037e5639dbfcf05f4
rellax-net <tsangandrazana@outlook.com>
2021-12-15 20:32:11 +0000

    Started zsh configuration

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
